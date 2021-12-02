6ba716df46af462232977987c66430ea32f4cfe9
Bartomiej Kuglin (SII SP. Z O. O. - POLAND) <Bartomiej.Kuglin@motorolasolutions.com>
2021-12-02 11:38:29 +0100

    DTCOM-16923 add support for rollback

--- src/core/github/listChangedFiles.js
+++ src/core/github/listChangedFiles.js
@@ -4,28 +4,62 @@ var util = require('util');
 
 var _config = require('../config');
 
-module.exports = function (revision, _currentVersion, callback) {
-  if (!_currentVersion) callback('there is no current version specified');
-  exec(
-    util.format('git rev-parse %s', _currentVersion),
-    { cwd: _config.dir.project_base },
-    function (error, out, err) {
-      if (error || err) {
-        callback(error || err);
-      }
-      var _currentVersionHash = out.split('\n')[0];
-      exec(
-        util.format('git diff --name-only %s...%s', _currentVersionHash, revision),
-        { cwd: _config.dir.project_base },
-        function (error, stdout, stderr) {
-          if (error || stderr) {
-            callback(error || stderr);
-          }
-
-          var changedFiles = stdout.split('\n');
-          callback(null, changedFiles);
+const getCommitHash = async (currentVersion, callback) => {
+  return new Promise(resolve => {
+    exec(
+      util.format('git rev-parse %s', currentVersion),
+      { cwd: _config.dir.project_base },
+      function (error, out, err) {
+        if (error || err) {
+          callback(error || err);
         }
-      );
-    }
-  );
+        resolve(out.split('\n')[0]);
+      }
+    );
+  });
+};
+
+const checkIfCommitIsAncestor = async (revision, currentVersionHash, callback) => {
+  return new Promise(resolve => {
+    exec(
+      util.format('git merge-base --is-ancestor %s %s; echo $?', revision, currentVersionHash),
+      { cwd: _config.dir.project_base },
+      function (error, stdout, stderr) {
+        if (error || stderr) {
+          callback(error || stderr);
+        }
+        const isAncestor = stdout.split('\n')[0] === '0' ? true : false;
+        resolve(isAncestor);
+      }
+    );
+  });
+};
+
+const getChangedFilesBetweenCommits = async (diffCommand, callback) => {
+  return new Promise(resolve => {
+    exec(
+      diffCommand,
+      { cwd: _config.dir.project_base },
+      function (error, stdout, stderr) {
+        if (error || stderr) {
+          callback(error || stderr);
+        }
+        resolve(stdout.split('\n'));
+      }
+    );
+  });
+};
+
+module.exports = async function (revision, currentVersion, callback) {
+  if (!currentVersion) callback('there is no current version specified');
+  const currentVersionHash = await getCommitHash(currentVersion, callback);
+  const isAncestor = await checkIfCommitIsAncestor(revision, currentVersionHash, callback);
+  let diffCommand;
+  if (isAncestor) {
+    diffCommand = util.format('git diff --name-only %s...%s', revision, currentVersionHash);
+  } else {
+    diffCommand = util.format('git diff --name-only %s...%s', currentVersionHash, revision);
+  }
+  const changedFiles = await getChangedFilesBetweenCommits(diffCommand, callback);
+  callback(null, changedFiles);
 };
