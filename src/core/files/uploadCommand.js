2ef82a64d97db58474e933bfb82cd9ca79884d11
Douglas Hipolito <douglas.hipolito@motorolasolutions.com>
2022-02-01 11:39:31 +0100

    Improving files command

--- src/core/files/uploadCommand.js
+++ src/core/files/uploadCommand.js
@@ -1,83 +1,17 @@
 'use strict';
 
 var fs = require('fs-extra');
-var util = require('util');
 var async = require('async');
 var path = require('path');
 var winston = require('winston');
-var Glob = require('glob').Glob;
-var webpack = require('webpack');
 var os = require('os');
 var _config = require('../config');
-var projectSettingsFiles = _config.projectSettings['files-config'] || [];
 var CleanCSS = require('clean-css');
+var { getFiles, generateFilePathMapping, resolveProjectFilesPaths, getFileSetting } = require('./utils');
+var jsBundle = require('./js-bundle');
 
 // Number of parallel uploads
 var PARALLEL_UPLOADS = 8;
-
-/**
- * Get all local files based on a glob pattern
- *
- * @param {String} globPattern the glob pattern
- * @param {Function} callback the callback function
- */
-function getFiles(globPattern, callback) {
-  var options = {
-    cwd: _config.dir.project_root,
-    absolute: true
-  };
-
-  var globCallback = function (error, fileList) {
-    if (error) {
-      return callback(error);
-    }
-
-    if (!fileList || !fileList.length) {
-      return callback(util.format('No file matching the pattern %s', globPattern));
-    }
-
-    callback(null, fileList);
-  };
-
-  new Glob(globPattern, options, globCallback);
-}
-
-function generateFilePathMapping(filePath, settingsFolder) {
-  const assetFilesPath = _config.dir.assetFilesPath;
-
-  const basePath = path.relative(assetFilesPath, filePath);
-  let folder = settingsFolder ? util.format('/%s/%s', settingsFolder, path.basename(filePath)) : path.dirname(basePath);
-  folder = folder.replace(/[\\]{1,2}/g, '/'); // win support
-
-  const baseFolder = basePath.split(path.sep)[0];
-  let thirdparty = true;
-  let remotePath = '';
-
-  if(baseFolder !== 'thirdparty') {
-    folder = baseFolder;
-    thirdparty = false;
-    remotePath = `/file/${basePath}`;
-  } else {
-    remotePath = basePath.replace(baseFolder, '').replace(/[\\]{1,2}/g, '/');
-  }
-
-  const remote = util.format(
-      '%s%s',
-      _config.environment.details.dns,
-      remotePath
-  );
-
-  let filename = settingsFolder ? folder : path.join(folder, path.basename(filePath));
-  filename = filename.replace(/[\\]{1,2}/g, '/'); // win support
-
-  return {
-    filename,
-    filePath,
-    folder,
-    thirdparty,
-    remote
-  }
-}
 
 /**
  * Upload a file list to OCC in parallel
@@ -189,28 +123,6 @@ function generateBundleTempFile({ source, fileSettings }, callback) {
   }
 }
 
-async function resolveProjectFilesPaths(callback) {
-  const getFilesPromisified = util.promisify(getFiles);
-
-  try {
-    for(const file of projectSettingsFiles) {
-      const foundFiles = await getFilesPromisified(file.path);
-      file.foundFiles = foundFiles;
-    }
-
-    callback(null);
-  } catch(error) {
-    callback(error);
-  }
-}
-
-function getFileSetting(source) {
-  return projectSettingsFiles.find(file => {
-    return file.foundFiles.includes(path.normalize(source));
-  })
-  || {};
-}
-
 /**
  * Actually upload the file to OCC
  *
@@ -262,90 +174,6 @@ function doFileUpload(source, destination, settings, token, callback) {
       });
     }
   ], callback);
-}
-
-/**
- * Bundle JS file
- * @param  {Object}   options Generate options
- * @param  {Function} done   on done the process
- */
-function jsBundle(options, done) {
-  var occToolsModulesPath = path.join(_config.occToolsPath, '..', 'node_modules');
-
-  var plugins = [];
-  plugins.push(new webpack.dependencies.LabeledModulesPlugin());
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    output: {
-      comments: false
-    }
-  }));
-
-  plugins.push(new webpack.DefinePlugin({
-    __ASSETS_VERSION__: `"${_config.assetsVersion}"`
-  }));
-
-  var entryFile = options.source;
-  var outputFile = options.tempFilePath;
-
-  var webpackConfigs = {
-    resolveLoader: {
-      root: [
-        occToolsModulesPath
-      ]
-    },
-    entry: entryFile,
-    output: {
-      path: options.dir,
-      filename: options.name,
-      libraryTarget: options.libraryTarget
-    },
-    externals: _config.webpackExternalsPattern,
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [
-          _config.dir.project_root
-        ],
-        query: {
-          presets: [path.join(occToolsModulesPath, 'babel-preset-es2015')],
-          plugins: [
-            path.join(occToolsModulesPath, 'babel-plugin-transform-decorators-legacy'),
-            path.join(occToolsModulesPath, 'babel-plugin-transform-class-properties'),
-            path.join(occToolsModulesPath, 'babel-plugin-transform-async-to-promises'),
-          ],
-          cacheDirectory: true
-        }
-      }]
-    },
-    plugins: plugins
-  };
-
-  var bundler = webpack(webpackConfigs);
-
-
-  bundler.run(function (error, stats) {
-    winston.info('[bundler:compile] %s', stats.toString({
-      chunks: true, // Makes the build much quieter
-      colors: true
-    }));
-
-    if (error) {
-      done(error, null);
-      return;
-    }
-
-    if (stats.hasErrors()) {
-      const statsErrors = stats.toJson().errors;
-      done(statsErrors.join(os.EOL + os.EOL), null);
-      return;
-    }
-
-    done(null, outputFile);
-  });
 }
 
 /**
